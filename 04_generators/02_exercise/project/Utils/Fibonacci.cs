85a6a775a8ce67445cf3c6d8faa231e177d0f784
pawel <pawcio27610@wp.pl>
2020-10-09 18:42:49 +0200

    fibonacci done

--- 04_generators/02_exercise/project/Utils/Fibonacci.cs
+++ 04_generators/02_exercise/project/Utils/Fibonacci.cs
@@ -5,9 +5,29 @@ namespace Utils
 {
     public class Fibonacci
     {
+        private int _a;
+        private int _b;
+        private int _count;
+
+        public Fibonacci()
+        {
+            _b = 1;
+            _a = 0;
+            _count = 0;
+        }
+
         public virtual IEnumerable<int> Numbers()
         {
-            // TODO: ...
+            while (_count<44)
+            {
+                _count++;
+                yield return _a;
+                _count++;
+                yield return _b;
+                _a += _b;
+                _b += _a;
+            }
+            throw new IndexOutOfRangeException();
         }
     }
 }
--- 04_generators/02_exercise/project/Utils/RangeFibonacci.cs
+++ 04_generators/02_exercise/project/Utils/RangeFibonacci.cs
@@ -4,13 +4,38 @@ namespace Utils
 {
     public class RangeFibonacci : Fibonacci
     {
-        // TODO: Fields....
-
-        // TODO: Constructor....
+        private readonly int _start;
+        private readonly int _step;
+        private readonly int _stop;
+        public RangeFibonacci(int start=0, int step=1, int stop=1)
+        {
+            _start = start;
+            _step = step;
+            _stop = stop;
+        }
 
         public override IEnumerable<int> Numbers()
         {
-            // TODO: Implement using version from base class....
+            var fibonacci = new Fibonacci();
+            var numbers = fibonacci.Numbers();
+            using var enumerator = numbers.GetEnumerator();
+
+            var i = 0;
+            for (; i <= _start; i++)
+            {
+                enumerator.MoveNext();
+            }
+
+            for (; i <= _stop; i += _step)
+            {
+                yield return enumerator.Current;
+                for (var j = 0; j < _step; j++)
+                {
+                    enumerator.MoveNext();
+                }
+            }
+
+            yield return enumerator.Current;
         }
     }
 }
