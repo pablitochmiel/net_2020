6cbfbff12f5555c4746baa75e43fcdbac3b780ce
pawel <pawcio27610@wp.pl>
2020-10-15 16:34:37 +0200

    add exercise 05

---
+++ 05_lambdas_delegates_and_events/02_exercise/project/App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Utils;
+
+namespace App
+{
+    internal static class Program
+    {
+        [ExcludeFromCodeCoverage]
+        private static void Main()
+        {
+            var window = new Window();
+
+            window.SimulateClicks();
+        }
+    }
+}
