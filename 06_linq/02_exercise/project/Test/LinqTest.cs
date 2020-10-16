d05ce5f50d832b4df7a90a741be649aa6ac14e82
pawel <pawcio27610@wp.pl>
2020-10-16 19:52:41 +0200

    zad done

--- 06_linq/02_exercise/project/Test/LinqTest.cs
+++ 06_linq/02_exercise/project/Test/LinqTest.cs
@@ -52,7 +52,7 @@ namespace Test
         [Fact]
         public void SelectAdultUsers()
         {
-            var result = /*TODO*/;
+            var result = from user  in _users where user.Age >18 select user;
 
             Assert.Equal(new[] {_users[1], _users[2], _users[3], _users[5]}, result);
         }
@@ -60,87 +60,89 @@ namespace Test
         [Fact]
         public void SelectAverageUserNameLengthForUsersWhoHaveName()
         {
-            var result = /*TODO*/;
-
+            var result = (from user in _users where !string.IsNullOrEmpty(user.Name)select user.Name!.Length).Average();
+        
             Assert.Equal(4.4, result, 2);
         }
-
+        
         [Fact]
         public void SelectCommentsWhereTitleIsLongerThanText()
         {
-            var result = /*TODO*/;
-
+            var result = from comment in _comments where comment.Text!.Length<comment.Title!.Length select comment;
+        
             Assert.Equal(new[] {_comments[4]}, result);
         }
-
+        
         [Fact]
         public void SelectOnlyUserNameForAdultUsers()
         {
-            var result = /*TODO*/;
-
+            var result = from user  in _users where user.Age >18 select user.Name;
+        
             Assert.Equal(new[] {_users[1].Name, _users[2].Name, _users[3].Name, _users[5].Name}, result);
         }
-
+        
         [Fact]
         public void SelectSumOfEvenNumbersAndSumOfOddNumbers()
         {
-            var result = /*TODO*/;
+            var result = new [] { (even :false, sum: (from number in _numbers where number%2==1 select number).Sum()) , (even :true, sum:(from number in _numbers where number%2==0 select number).Sum()) };
 
             var resultArray = result.ToArray();
             Assert.Equal(2, resultArray.Length);
             Assert.Equal(resultArray[0], (even: false, sum: 39));
             Assert.Equal(resultArray[1], (even: true, sum: 20));
         }
-
+        
         [Fact]
         public void SelectTotalNumberOfCharactersInAllWords()
         {
-            var result = /*TODO*/;
-
+            var result = (from word in _words select word.Length).Sum();
+        
             Assert.Equal(36, result);
         }
-
+        
         [Fact]
         public void SelectUserCommentsForPostsWithNoTitle()
         {
-            var result = /*TODO*/;
+            var result = from comment in _comments join t in from user in _users
+            join post in _posts on user.Id equals post.UserId
+                select new {user, post} on comment.PostId equals t.post.Id where string.IsNullOrEmpty(t.post.Title) select (t.user,comment);
 
             var resultArray = result.ToArray();
             Assert.Equal(2, resultArray.Length);
             Assert.Equal(resultArray[0], (_users[3], _comments[2]));
             Assert.Equal(resultArray[1], (_users[3], _comments[3]));
         }
-
+        
         [Fact]
         public void SelectUsersWherePostTitleStartsWithUppercaseLetter()
         {
-            var result = /*TODO*/;
+            var result = from user in _users join post in _posts on user.Id equals post.UserId where !string.IsNullOrEmpty(post.Title) && char.IsUpper(post.Title![0]) select user;
             
             Assert.Equal(new[] {_users[0], _users[3]}, result);
         }
-
+        
         [Fact]
         public void SelectUsersWithNameLongerThanThreeCharacters()
         {
-            var result = /*TODO*/;
-
+            var result = from user  in _users where !string.IsNullOrEmpty(user.Name) && user!.Name.Length >3 select user;
+        
             Assert.Equal(new[] {_users[0], _users[3], _users[4]}, result);
         }
-
+        
         [Fact]
         public void SelectUserWithNameTom()
         {
-            var result = /*TODO*/;
-
+            var result = from user  in _users where !string.IsNullOrEmpty(user.Name) && user!.Name == "Tom" select user;
+        
             Assert.Equal(new[] {_users[1]}, result);
         }
-
+        
         [Fact]
         public void SelectThreeLongestWordsInDescendingOrder()
         {
-            var result = /*TODO*/;
-            
-            Assert.Equal(new []{"dotnet", "house", "plane"}, result);
+            var result =  (from word in _words orderby word.Length descending select word).Take(3);
+        
+            Assert.Equal(new[] {"dotnet", "house", "plane"}, result);
         }
     }
 }
