4e26224794f19b89558150bbd8c33a6815a1df71
pawel <pawcio27610@wp.pl>
2020-10-25 21:47:15 +0100

    zad 7 done

--- 07_multithreading/02_exercise/project/Utils/Demo.cs
+++ 07_multithreading/02_exercise/project/Utils/Demo.cs
@@ -17,22 +17,28 @@ namespace Utils
         public int Sum()
         {
             // TODO: Use for loop to calculate sum
+            var res = 0;
+            for (var i = 0; i < _data.Length; i++)
+                res += _data[i];
 
-            return 0;
+            return res;
         }
 
         public int SumForeach()
         {
             // TODO: Use foreach loop to calculate sum
-
-            return 0;
+            var res = 0;
+            foreach (var t in _data)
+                res += t;
+            
+            return res;
         }
 
         public int SumLinq()
         {
             // TODO: Use for LINQ to calculate sum
 
-            return 0;
+            return _data.Sum();
         }
 
         private delegate void ThreadAction(int start, int stop);
@@ -40,6 +46,21 @@ namespace Utils
         private void RunStandaloneThreads(int count, ThreadAction action)
         {
             // TODO: Run 'count' threads and execute 'action' in every one with appropriate range of data
+            var t = new Thread[count];
+            for (var i = 0; i < count; i++)
+            {
+                var i1 = i;
+                t[i] = new Thread(() => action(i1*_data.Length / count, (i1+1)*_data.Length / count));
+            }
+
+            foreach (var temp in t)
+            {
+                temp.Start();
+            } 
+            foreach (var temp in t)
+            {
+                temp.Join();
+            }
         }
 
         public int SumThreadsInterlocked(int count)
@@ -47,8 +68,14 @@ namespace Utils
             // TODO: Use 'Interlocked.Add' to calculate total sum
             // TODO: Use 'RunStandaloneThreads' to run threads
             // TODO: Ue lambda to poss 'action' to 'RunStandaloneThreads'
+            var sum = 0;
+            RunStandaloneThreads(count, (start, stop) =>
+            {
+                for (var i = start; i < stop; i++)
+                    Interlocked.Add(ref sum, _data[i]);                                                                                                                                                                            
+            });                                                        
 
-            return 0;
+            return sum;
         }
 
 
@@ -58,14 +85,33 @@ namespace Utils
             // TODO: Use partial sum in threads to avoid excessive use of 'Interlocked.Add'
             // TODO: Use 'RunStandaloneThreads' to run threads
             // TODO: Ue lambda to poss 'action' to 'RunStandaloneThreads'
+            var sum = 0;
+            RunStandaloneThreads(count, (start, stop) =>
+            {
+                var s = 0;
+                for (var i = start; i < stop; i++)
+                {
+                    s += _data[i];
+                }
+                Interlocked.Add(ref sum, s);
+            });
 
-            return 0;
+            return sum;
         }
 
         private void RunPoolThreads(int count, ThreadAction action)
         {
             // TODO: Run 'count' pool threads and execute 'action' in every one with appropriate range of data
             // TODO: Use 'ManualResetEvent' to synchronize operations
+            var events = new List<ManualResetEvent>();
+            for (int i = 0; i < count; i++)
+            {
+                var i1 = i;
+                var resetEvent=new ManualResetEvent(false);
+                ThreadPool.QueueUserWorkItem(_ => { action(i1*_data.Length / count, (i1+1)*_data.Length / count); resetEvent.Set(); });
+                events.Add(resetEvent);
+            }
+            WaitHandle.WaitAll(events.ToArray<WaitHandle>());
         }
 
         public int SumPoolThreads(int count)
@@ -73,16 +119,41 @@ namespace Utils
             // TODO: Use 'RunPoolThreads' to run threads
             // TODO: Use 'Interlocked.Add' to aggregate data
             // TODO: Ue lambda to poss 'action' to 'RunPoolThreads'
+            // var events = new List<ManualResetEvent>();
+            // for (int i = 0; i < 10; i++) {
+            //     var resetEvent = new ManualResetEvent(false);
+            //     ThreadPool.QueueUserWorkItem(_ => { ThreadAction(); resetEvent.Set(); });
+            //     events.Add(resetEvent);
+            // }
+            // WaitHandle.WaitAll(events.ToArray<WaitHandle>());
+            
+            var sum = 0;
+            RunPoolThreads(count,(start, stop)=>
+            {
+                var s = 0;
+                for (var i = start; i < stop; i++)
+                {
+                    s += _data[i];
+                }
+                Interlocked.Add(ref sum, s);
+            });
 
-            return 0;
+            return sum;
         }
 
         public int SumTpl()
         {
             // TODO: Use 'Parallel.For' to calculate sum
             // TODO: Ue lambdas to poss operations
-
-            return 0;
+            var sum = 0;
+            Parallel.For(0, _data.Length, () => 0, (j, loop, subtotal) =>
+                {
+                    subtotal += _data[j];
+                    return subtotal;
+                },
+                (x) => Interlocked.Add(ref sum, x)
+            );
+            return  sum;
         }
     }
 }
