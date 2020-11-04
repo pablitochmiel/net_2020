cc0688c76c1b60eaeafedba74aca4c1951504159
pawel <pawcio27610@wp.pl>
2020-11-04 22:21:55 +0100

    engine pass tests and maybe works

--- 08_async/02_exercise/project/Utils/Engine.cs
+++ 08_async/02_exercise/project/Utils/Engine.cs
@@ -12,22 +12,58 @@ namespace Utils
         private readonly Dictionary<Task<string>, Command> _taskCommands;
         private readonly List<Task<string>> _tasks;
 
-        public Engine(ICommandFactory factoryMockObject)
+        public Engine(ICommandFactory? factoryMockObject)
         {
             _commands=new List<Command>();
-            _commandFactory = factoryMockObject;
+            _commandFactory = factoryMockObject ?? throw new NullReferenceException();
             _tasks=new List<Task<string>>();
             _taskCommands=new Dictionary<Task<string>, Command>();
         }
 
         public async Task RunAsync()
         {
+            var inputCommand = _commandFactory.Create("input");
+            Console.WriteLine("[+] " + inputCommand.Name);
+            var task = inputCommand.RunAsync(_commandFactory.CancellationTokenSource.Token);
+            
+            _tasks.Add(task);
+            _commands.Add(inputCommand);
+            _taskCommands.Add(task,inputCommand);
+            
             while (_tasks.Count > 0)
             {
-                var task = await Task.WhenAny(_tasks);
-                _tasks.Remove(task);
-                var result = await task;
-                Console.WriteLine(result);
+                task = await Task.WhenAny(_tasks);
+                try
+                {
+                    string result = await task;
+                    inputCommand = _taskCommands[task];
+                    Console.WriteLine("[-] " + inputCommand.Name);
+
+                    if (!string.IsNullOrEmpty(result)) Console.WriteLine("[=] " + result);
+                    
+                    var commands = inputCommand.Continuations(result, _commandFactory);
+                    foreach (var command in commands)
+                    {
+                        Console.WriteLine("[+] " + command.Name);
+                        Task<string> runTask = command.RunAsync(_commandFactory.CancellationTokenSource.Token);
+                        _tasks.Add(runTask);
+                        _commands.Add(command);
+                        _taskCommands.Add(runTask,command);
+                    }
+
+                    _tasks.Remove(task);
+                    _commands.Remove(_taskCommands[task]);
+                    _taskCommands.Remove(task);
+                }
+                catch (TaskCanceledException)
+                {
+                    var command = _taskCommands[task];
+                    Console.WriteLine("[-] "+inputCommand.Name);
+                    Console.WriteLine("[X] "+inputCommand.Name);
+                    if (_commands.Contains(command)) _commands.Remove(command);
+                    _tasks.Remove(task);
+                    _taskCommands.Remove(task);
+                }
             }
         }
     }
